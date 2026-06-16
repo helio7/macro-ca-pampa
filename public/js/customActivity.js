e54b9e79edb8b09c295bd4de52f153bc8a1be76b
Dylan Haffner <lu9fdn@gmail.com>
2026-06-16 20:12:07 +0000

    Improvement

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -18,7 +18,7 @@ define(['postmonger'], function (Postmonger) {
   function serializeObject(obj) {
     return Object.keys(obj)
       .map(function (key) {
-        return key + '=' + obj[key];
+        return key + '=' + encodeURIComponent(obj[key]);
       })
       .join(';');
   }
@@ -29,10 +29,31 @@ define(['postmonger'], function (Postmonger) {
     str.split(';').forEach(function (pair) {
       var parts = pair.split('=');
       var key = parts.shift();
-      result[key] = parts.join('=');
+
+      if (!key) {
+        return;
+      }
+
+      result[key] = decodeURIComponent(parts.join('='));
     });
 
     return result;
+  }
+
+  function getColumnNameFromAttribute(value) {
+    if (!value || typeof value !== 'string') {
+      return '';
+    }
+
+    return value.split('.').pop().replace('}}', '');
+  }
+
+  function buildContactAttribute(dataExtension, columnName) {
+    if (!dataExtension || !columnName) {
+      return null;
+    }
+
+    return '{{Contact.Attribute."' + dataExtension + '".' + columnName + '}}';
   }
 
   function addVariableRow(value) {
@@ -70,13 +91,28 @@ define(['postmonger'], function (Postmonger) {
         byId('templateId').value = argument.templateId;
       }
 
+      if (argument.dni) {
+        byId('dni').value = getColumnNameFromAttribute(argument.dni);
+      }
+
+      if (argument.gender) {
+        byId('gender').value = getColumnNameFromAttribute(argument.gender);
+      }
+
+      if (argument.tributario) {
+        byId('tributario').value = getColumnNameFromAttribute(argument.tributario);
+      }
+
+      if (argument.bsuid) {
+        byId('bsuid').value = getColumnNameFromAttribute(argument.bsuid);
+      }
+
       if (argument.variables && argument.variables !== 'NO_VARIABLES') {
         var parsedVariables = deserializeString(argument.variables);
 
         Object.keys(parsedVariables).forEach(function (key) {
           var value = parsedVariables[key];
-          var columnName = value.split('.').pop().replace('}}', '');
-          addVariableRow(columnName);
+          addVariableRow(getColumnNameFromAttribute(value));
         });
       }
     });
@@ -87,15 +123,22 @@ define(['postmonger'], function (Postmonger) {
     var phoneColumnName = byId('dataExtensionPhoneNumberColumnName').value;
     var campaignName = byId('campaignName').value;
     var templateId = byId('templateId').value;
-    var phoneNumber = '{{Contact.Attribute."' + dataExtension + '".' + phoneColumnName + '}}';
+    var dniColumnName = byId('dni').value;
+    var genderColumnName = byId('gender').value;
+    var tributarioColumnName = byId('tributario').value;
+    var bsuidColumnName = byId('bsuid').value;
+    var phoneNumber = buildContactAttribute(dataExtension, phoneColumnName);
+    var dni = buildContactAttribute(dataExtension, dniColumnName);
+    var gender = buildContactAttribute(dataExtension, genderColumnName);
+    var tributario = buildContactAttribute(dataExtension, tributarioColumnName);
+    var bsuid = buildContactAttribute(dataExtension, bsuidColumnName);
     var variables = {};
     var rows = document.querySelectorAll('.variable-item');
 
     rows.forEach(function (row) {
       var input = row.querySelector('input');
       var variableNumber = row.id.replace('group-', '');
-      variables[variableNumber] =
-        '{{Contact.Attribute."' + dataExtension + '".' + input.value + '}}';
+      variables[variableNumber] = buildContactAttribute(dataExtension, input.value);
     });
 
     return [
@@ -105,10 +148,14 @@ define(['postmonger'], function (Postmonger) {
       },
       { campaignName: campaignName || null },
       { templateId: templateId || null },
-      { phoneNumber: dataExtension && phoneColumnName ? phoneNumber : null },
       {
         variables: rows.length ? serializeObject(variables) : 'NO_VARIABLES'
-      }
+      },
+      { phoneNumber: phoneNumber },
+      { dni: dni },
+      { gender: gender },
+      { tributario: tributario },
+      { bsuid: bsuid }
     ];
   }
 
