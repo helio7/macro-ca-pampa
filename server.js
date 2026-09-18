8155eebfc8d9d80425e3ec8b2d78b86fc0d46f98
Dylan Haffner <lu9fdn@gmail.com>
2026-09-18 11:58:24 +0000

    Versión 15

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const port = Number(process.env.PORT || 3000);
 const publicDir = path.join(__dirname, 'public');
 const configTemplatePath = path.join(publicDir, 'config.template.json');
 
-const VERSION = 14;
+const VERSION = 15;
 
 const basePath = `/version${VERSION}`;
 
